dcd50d6a385c7b558ac04122887f5c41c0b7df06
Parberry, Ian <Ian.Parberry@unt.edu>
2022-03-11 12:20:46 -0600

    Update default tiles and documentation.

--- docs/html/search/all_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_7ecmain_52',['~CMain',['../class_c_main.html#a4fe4429d06d7bcba0fbf8a19c29d0488',1,'CMain']]],
-  ['_7ecwangtiler_53',['~CWangTiler',['../class_c_wang_tiler.html#a39d50ef0cd5473552cb5652cde634c98',1,'CWangTiler']]]
-];
--- docs/html/search/files_4.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['wangtiler_2ecpp_63',['WangTiler.cpp',['../_wang_tiler_8cpp.html',1,'']]],
-  ['wangtiler_2eh_64',['WangTiler.h',['../_wang_tiler_8h.html',1,'']]],
-  ['windowshelpers_2ecpp_65',['WindowsHelpers.cpp',['../_windows_helpers_8cpp.html',1,'']]],
-  ['windowshelpers_2eh_66',['WindowsHelpers.h',['../_windows_helpers_8h.html',1,'']]]
-];
