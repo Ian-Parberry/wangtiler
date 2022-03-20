637fe384ea81bfbf30f22c848bed354ca51ae456
Parberry, Ian <Ian.Parberry@unt.edu>
2022-03-20 13:18:26 -0500

    Update documentation.

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -3,11 +3,13 @@ var searchData=
   ['idm_5ffile_5fgenerate_0',['IDM_FILE_GENERATE',['../_windows_helpers_8h.html#a9fc7ffbe039da67fe10025bfe6731b8e',1,'WindowsHelpers.h']]],
   ['idm_5ffile_5fquit_1',['IDM_FILE_QUIT',['../_windows_helpers_8h.html#a9a7a0891436ce76ac27b86ba302558ed',1,'WindowsHelpers.h']]],
   ['idm_5ffile_5fsave_2',['IDM_FILE_SAVE',['../_windows_helpers_8h.html#a65e62ad25e0b57e2f4b59cab2cedfa4b',1,'WindowsHelpers.h']]],
-  ['idm_5ftileset_5fdefault_3',['IDM_TILESET_DEFAULT',['../_windows_helpers_8h.html#af1c10acfd59f30182cd3f4783bc5dc65',1,'WindowsHelpers.h']]],
-  ['idm_5ftileset_5fflower_4',['IDM_TILESET_FLOWER',['../_windows_helpers_8h.html#a533cfe101d1f8ae284828976b4d257e9',1,'WindowsHelpers.h']]],
-  ['idm_5ftileset_5fgrass_5',['IDM_TILESET_GRASS',['../_windows_helpers_8h.html#a77748248e731a0d490616f5333c97468',1,'WindowsHelpers.h']]],
-  ['idm_5ftileset_5fmud_6',['IDM_TILESET_MUD',['../_windows_helpers_8h.html#adf7a69cf79223a97c62ffb7f0cc3677f',1,'WindowsHelpers.h']]],
-  ['includes_2eh_7',['Includes.h',['../_includes_8h.html',1,'']]],
-  ['initgdiplus_8',['InitGDIPlus',['../_windows_helpers_8cpp.html#ab90ef52869992f4af6696021ce4a1769',1,'InitGDIPlus():&#160;WindowsHelpers.cpp'],['../_windows_helpers_8h.html#ab90ef52869992f4af6696021ce4a1769',1,'InitGDIPlus():&#160;WindowsHelpers.cpp']]],
-  ['initwindow_9',['InitWindow',['../_windows_helpers_8cpp.html#ad68cbf778113a9ec082a5090cb2703c0',1,'InitWindow(HINSTANCE hInst, INT nShow, WNDPROC WndProc):&#160;WindowsHelpers.cpp'],['../_windows_helpers_8h.html#af0de5efedf2d3b9a5e0dbabcd61e8d35',1,'InitWindow(HINSTANCE, INT, WNDPROC):&#160;WindowsHelpers.cpp']]]
+  ['idm_5fhelp_5fabout_3',['IDM_HELP_ABOUT',['../_windows_helpers_8h.html#ae6f93574b34e7fa677a9cb2e63f94bd1',1,'WindowsHelpers.h']]],
+  ['idm_5fhelp_5fhelp_4',['IDM_HELP_HELP',['../_windows_helpers_8h.html#aba5b0e56f9fc089fedb152863012cbfc',1,'WindowsHelpers.h']]],
+  ['idm_5ftileset_5fdefault_5',['IDM_TILESET_DEFAULT',['../_windows_helpers_8h.html#af1c10acfd59f30182cd3f4783bc5dc65',1,'WindowsHelpers.h']]],
+  ['idm_5ftileset_5fflower_6',['IDM_TILESET_FLOWER',['../_windows_helpers_8h.html#a533cfe101d1f8ae284828976b4d257e9',1,'WindowsHelpers.h']]],
+  ['idm_5ftileset_5fgrass_7',['IDM_TILESET_GRASS',['../_windows_helpers_8h.html#a77748248e731a0d490616f5333c97468',1,'WindowsHelpers.h']]],
+  ['idm_5ftileset_5fmud_8',['IDM_TILESET_MUD',['../_windows_helpers_8h.html#adf7a69cf79223a97c62ffb7f0cc3677f',1,'WindowsHelpers.h']]],
+  ['includes_2eh_9',['Includes.h',['../_includes_8h.html',1,'']]],
+  ['initgdiplus_10',['InitGDIPlus',['../_windows_helpers_8cpp.html#ab90ef52869992f4af6696021ce4a1769',1,'InitGDIPlus():&#160;WindowsHelpers.cpp'],['../_windows_helpers_8h.html#ab90ef52869992f4af6696021ce4a1769',1,'InitGDIPlus():&#160;WindowsHelpers.cpp']]],
+  ['initwindow_11',['InitWindow',['../_windows_helpers_8cpp.html#ad68cbf778113a9ec082a5090cb2703c0',1,'InitWindow(HINSTANCE hInst, INT nShow, WNDPROC WndProc):&#160;WindowsHelpers.cpp'],['../_windows_helpers_8h.html#af0de5efedf2d3b9a5e0dbabcd61e8d35',1,'InitWindow(HINSTANCE, INT, WNDPROC):&#160;WindowsHelpers.cpp']]]
 ];
